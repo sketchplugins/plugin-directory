2c828b41a3c7742e46d0d291d41a99540ccb1b02
pravdomil <pravdomil.toman@gmail.com>
2018-05-26 20:51:55 +0200

    comments

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,13 @@
 type SketchDirectory = SketchPlugin[];
 
 type SketchPlugin = {
-  title: string, // from manifest.json
-  description: string, // from manifest.json
-  author: string, // from manifest.json
-  homepage: string, // from manifest.json
-  appcast?: string, // from manifest.json
+  title: string,        // name from manifest.json non-empty string,
+  description: string,  // from manifest.json
+  author: string,       // from manifest.json non-empty string
+  homepage: string,     // from manifest.json non-empty string
+  appcast?: string,     // from manifest.json
   
-  owner?: string, // GitHub user name
-  name?: string, // GitHub repo name
+  owner?: string,       // GitHub user name
+  name?: string,        // GitHub repo name
   lastUpdated?: string, // last commit date
 };
