c6dfc52e435870f5cc25f14d264c195ebf101776
pravdomil <pravdomil.toman@gmail.com>
2018-05-26 20:55:00 +0200

    minor

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 type SketchDirectory = SketchPlugin[];
 
 type SketchPlugin = {
-  title: string,        // name from manifest.json non-empty string,
+  title: string,        // name from manifest.json non-empty string
   description: string,  // from manifest.json
   author: string,       // from manifest.json non-empty string
   homepage: string,     // from manifest.json non-empty string
