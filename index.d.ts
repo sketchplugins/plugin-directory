4c4681b61cc64502d9c32f123b610a6627c6ca0f
pravdomil <pravdomil.toman@gmail.com>
2018-05-26 15:57:32 +0200

    update typings to match readme

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,11 @@
 type SketchPlugin = {
-  owner: string, // GitHub user name
-  name: string, // GitHub repo name
-  lastUpdated: string, // last commit date
-  
   title: string, // from manifest.json
   description: string, // from manifest.json
-  appcast: string, // from manifest.json
-  homepage: string, // from manifest.json
   author: string, // from manifest.json
+  homepage: string, // from manifest.json
+  appcast?: string, // from manifest.json
+  
+  owner?: string, // GitHub user name
+  name?: string, // GitHub repo name
+  lastUpdated?: string, // last commit date
 };
