50f544a26f8f9752e2985d2e2326dd232f746e44
pravdomil <pravdomil.toman@gmail.com>
2018-05-26 13:16:12 +0200

    getPlugins

--- src/build.js
+++ src/build.js
@@ -1,3 +1,18 @@
+/**
+ * @returns {Promise<SketchPlugin[]>}
+ */
+async function getPlugins() {
+  try {
+    const result = JSON.parse(await readFile("plugins.json", "utf8"))
+    if (!(result instanceof Object)) {
+      throw new Error()
+    }
+    return result
+  } catch {
+    throw new Error("Error occurred while reading plugins.json")
+  }
+}
+
 async function savePlugins(plugins) {
   try {
     await writeFile("plugins.json", JSON.stringify(plugins))
