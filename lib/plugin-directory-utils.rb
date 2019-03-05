0e15b02f7936790ae085033017973852acffcd3b
Ale M <bomberstudios@gmail.com>
2019-03-05 11:17:37 +0100

    Improve titlefy method

--- lib/plugin-directory-utils.rb
+++ lib/plugin-directory-utils.rb
@@ -1,6 +1,7 @@
 # This is used on the titlefy function. The idea here is to ignore some word that should never be
 # re-capitalised
 IGNORE = %w(the of a and AE RTL PS HTML UI SF JSON SCSS px RGB HSL HEX iOS iPhone iPad VR SVGO SketchContentSync LayerRenamer SketchRunner Gridy Looper SizeArtboard Shapr)
+UPCASE = %w(rtl ps html ui sf json scss rgb hsl hex vr svgo pdf png sd ds afux)
 
 def titlefy string
   if IGNORE.include? (string)
@@ -10,17 +11,19 @@ def titlefy string
   if s.count == 1
     return string
   end
+
   # puts "Words: #{s}"
   s.map do |word|
     word_lowercase = word.downcase
     if IGNORE.include?(word)
       word
-    # elsif IGNORE.include?(word_lowercase)
-    #   word_lowercase
+    elsif UPCASE.include? (word.downcase)
+      word.upcase
     else
       word.capitalize!
     end
   end
-  s.join(' ')
+  title = s.join(' ')
+  puts title
+  return title
 end
-
